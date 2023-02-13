d70862e66cac9a5defee6d661371f6cca74ece47
famin3 <xfamin3@gmail.com>
2023-02-13 00:36:28 +0000

    auth0 auth added

---
+++ CAVU.Api/AllowAnonymous.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace CAVU.Api;
+
+public class AllowAnonymous : IAuthorizationHandler
+{
+    public Task HandleAsync(AuthorizationHandlerContext context)
+    {
+        foreach (IAuthorizationRequirement requirement in context.PendingRequirements.ToList())
+            context.Succeed(requirement); 
+        
+        return Task.CompletedTask;
+    }
+}
--- CAVU.Api/Program.cs
+++ CAVU.Api/Program.cs
@@ -1,8 +1,12 @@
 using System.ComponentModel;
+using CAVU.Api;
 using CAVU.CarParkService;
 using CAVU.CarParkService.DTOs;
 using CAVU.CarParkService.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,9 +15,62 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "CAVU",
+        Version = "v1"
+    });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 123asd\"",
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme {
+                Reference = new OpenApiReference {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 builder.Services.AddDbContext<CarParkContext>(opt => opt.UseInMemoryDatabase("CAVU"));
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.Authority = builder.Configuration["Auth0:Domain"];
+    options.Audience = builder.Configuration["Auth0:Audience"];
+});
+
+builder.Services.AddAuthorization(o =>
+{
+    o.AddPolicy("read:all", p => p.
+        RequireAuthenticatedUser().
+        RequireClaim("permissions", "read:all"));
+});
+
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddSingleton<IAuthorizationHandler, AllowAnonymous>();
+}
+
+
+
 
 var app = builder.Build();
 
@@ -23,7 +80,7 @@ MockedData.AddMockedData(app);
 
 
 app.MapGet("/booking", async (CarParkContext context) =>
-    Results.Json(await context.Bookings.ToListAsync()));
+    Results.Json(await context.Bookings.ToListAsync())).RequireAuthorization("read:all");
 
 app.MapGet("/booking/availableslotsfordaterange", async ([DefaultValue("2023-06-01")]DateTime from, [DefaultValue("2023-06-30")]DateTime to, CarParkContext context) =>
 {
@@ -31,7 +88,7 @@ app.MapGet("/booking/availableslotsfordaterange", async ([DefaultValue("2023-06-
     var takenSlots = await context.Bookings.Where(x => x.StartDate.Date <= to && x.EndDate.Date >= from && x.Active)
         .Select(y => y.ParkingSpotId).ToListAsync();
     return Results.Json(parkingSlots.Except(takenSlots));
-});
+}).RequireAuthorization("read:all");
 
 app.MapGet("/booking/availableslotscountperday", async ([DefaultValue("2023-06-01")]DateTime from, [DefaultValue("2023-06-30")]DateTime to, CarParkContext context) =>
 {
@@ -52,14 +109,14 @@ app.MapGet("/booking/availableslotscountperday", async ([DefaultValue("2023-06-0
     }
 
     return Results.Json(result);
-});
+}).RequireAuthorization("read:all");
 
 app.MapGet("/booking/price", async ([DefaultValue("2023-06-01")]DateTime from, [DefaultValue("2023-06-30")]DateTime to, CarParkContext context) =>
 {
     var prices = await context.Prices.ToListAsync();
     var result = PriceCalculator.Calculate(prices, DateOnly.FromDateTime(from), DateOnly.FromDateTime(to));
     return Results.Json(result);
-});
+}).RequireAuthorization("read:all");
 
 app.MapPost("/booking/", async (Booking booking, CarParkContext context) =>
 {
@@ -74,7 +131,7 @@ app.MapPost("/booking/", async (Booking booking, CarParkContext context) =>
     context.Bookings.Add(booking);
     await context.SaveChangesAsync();
     return Results.Ok();
-});
+}).RequireAuthorization("read:all");
 
 app.MapPut("/booking/cancel", async (int id, CarParkContext context) =>
 {
@@ -87,7 +144,7 @@ app.MapPut("/booking/cancel", async (int id, CarParkContext context) =>
 
     return Results.NotFound();
 
-});
+}).RequireAuthorization("read:all");
 
 app.MapPut("/booking/", async (Booking dto, CarParkContext context) =>
 {
@@ -97,14 +154,14 @@ app.MapPut("/booking/", async (Booking dto, CarParkContext context) =>
     await context.SaveChangesAsync();
     return Results.Ok();
 
-});
+}).RequireAuthorization("read:all");
 
 
 app.MapGet("/parkingspot", async (CarParkContext context) =>
 {
     var result = (await context.ParkingSpots.ToListAsync()).Select(ParkingSpotDto.From).ToList();
     return Results.Json(result);
-});
+}).RequireAuthorization("read:all");
    
 
 app.MapPost("/parkingspot", async (ParkingSpot parkingSpot, CarParkContext context) =>
@@ -112,7 +169,7 @@ app.MapPost("/parkingspot", async (ParkingSpot parkingSpot, CarParkContext conte
     context.ParkingSpots.Add(parkingSpot);
     await context.SaveChangesAsync();
     return Results.Ok();
-});
+}).RequireAuthorization("read:all");
 
 app.MapDelete("/parkingspot{id}", async (int id, CarParkContext context) =>
 {
@@ -124,19 +181,15 @@ app.MapDelete("/parkingspot{id}", async (int id, CarParkContext context) =>
     }
 
     return Results.NotFound();
-});
+}).RequireAuthorization("read:all");
 
 
 app.MapGet("/price", async (CarParkContext context) =>
 {
     var result = (await context.Prices.ToListAsync()).Select(PriceDto.CreateFrom).ToList();
     return Results.Json(result);
-});
+}).RequireAuthorization("read:all");
 
-
-
-
-   
 
 
 
@@ -150,9 +203,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+
 
 app.MapControllers();
 
